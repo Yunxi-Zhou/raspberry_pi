4fc65cbdbfef70f0ab764fc6c138b8a887b855ff
Ethan Raspberry <zhouyunxi1115@gmail.com>
2024-10-23 21:31:25 +0100

    LCD

---
+++ src/lcd.py
@@ -0,0 +1,15 @@
+import LCD1602
+import time
+
+def setup():
+    LCD1602.init(0x27,1)
+    LCD1602.write(0,0,'Greetings!')
+    LCD1602.write(1,1,'From Ethan')
+    time.sleep(1)
+
+try:
+    setup()
+except KeyboardInterrupt:
+    LCD1602.clear()
+finally:
+    pass
